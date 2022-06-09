b2c24debd1fb552395cbcecc2522f0134f9b08d9
Dino Krtanjek <krtanjekdino@gmail.com>
2022-06-09 18:56:57 +0200

    Fixed centering if even number of items

--- src/hooks/useViewport.ts
+++ src/hooks/useViewport.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 
 function useViewport(
@@ -7,9 +7,16 @@ function useViewport(
   itemsPerPage: number
 ): [ number, (index: number) => void ] {
   const [ firstIndex, setFirstIndex ] = useState(activeIndex);
+  const lastActiveIndex = useRef(activeIndex);
 
   useEffect(() => {
-    const placesBeforeCenter = Math.floor((itemsPerPage - 1) / 2);
+    let placesBeforeCenter = Math.floor((itemsPerPage - 1) / 2);
+
+    if (lastActiveIndex.current < activeIndex) {
+      // going up
+      placesBeforeCenter = Math.ceil((itemsPerPage - 1) / 2);
+    }
+
     let firstIndex = activeIndex - placesBeforeCenter;
 
     if (firstIndex + itemsPerPage > childrenCount) {
@@ -22,6 +29,10 @@ function useViewport(
 
     setFirstIndex(firstIndex);
   }, [ itemsPerPage, childrenCount, activeIndex ]);
+
+  useEffect(() => {
+    lastActiveIndex.current = activeIndex;
+  }, [ activeIndex ]);
 
   return [ firstIndex, setFirstIndex ];
 }
