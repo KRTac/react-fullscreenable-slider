1e3f318d34c6c8816ac4f5c8b115cdbcd2fa4f80
Dino Krtanjek <krtanjekdino@gmail.com>
2022-06-10 23:16:45 +0200

    Added Storybook

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,11 @@
+const repoName = 'react-responsive-slider';
+
 module.exports = {
   siteMetadata: {
     title: 'React Responsive Slider',
-    siteUrl: 'https://krtac.github.io/react-responsive-slider/'
+    siteUrl: `https://krtac.github.io/${repoName}/`
   },
+  pathPrefix: `/${repoName}`,
   plugins: [
     'gatsby-plugin-image',
     'gatsby-plugin-react-helmet',
