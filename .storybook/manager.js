23414b4e921286cb3c1dae1a355fb2d4795d2946
Dino Krtanjek <krtanjekdino@gmail.com>
2022-06-11 04:16:11 +0200

    Updated storybook sidebar image

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -2,4 +2,4 @@ import { addons } from '@storybook/addons';
 import { themes } from '@storybook/theming';
 
 
-addons.setConfig({ theme: themes.dark });
+addons.setConfig({ theme: { ...themes.dark, brandImage: 'https://img.shields.io/badge/React-Responsive%20Slider-%23d47e33?labelColor=82b80d&style=for-the-badge' } });
